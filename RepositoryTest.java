43711e78e0af9b7be7d24b8e9362d82172b48cf9
Peter <9016605+brichzin@users.noreply.github.com>
2020-04-20 14:03:51 +0200

    Eine Java Datei zum Testen des Repositorys

---
+++ RepositoryTest.java
@@ -0,0 +1,32 @@
+
+/**
+ * Beschreiben Sie hier die Klasse RepositoryTest.
+ * 
+ * @author (Peter Brichzin) 
+ * @version (20.4.20)
+ */
+class RepositoryTest
+{
+    String pfad;
+
+    /**
+     * Konstruktor für Objekte der Klasse RepositoryTest
+     */
+    RepositoryTest()
+    {
+        // Instanzvariable initialisieren
+        pfad = "https://github.com/brichzin/SeminarInformatik";
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    String Begruessen()
+    {
+        
+        return "Herzlich Willkommen";
+    }
+}
