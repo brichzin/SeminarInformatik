43b379e9730e453fdb4e1ce32a04b7120d1c6b94
lsenka <lenka.senajova@gmail.com>
2020-04-22 12:14:15 +0200

    neue Klassen

---
+++ Model.java
@@ -0,0 +1,30 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Model.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Model
+{
+    
+
+    /**
+     * Konstruktor für Objekte der Klasse Model
+     */
+    public Model()
+    {
+       
+    }
+
+    
+    public void datenManuellEingeben()
+    {
+        
+    }
+    
+    public void datenAuslesen()
+    {
+        
+    }
+}
---
+++ View.java
@@ -0,0 +1,29 @@
+
+/**
+ * Beschreiben Sie hier die Klasse View.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class View
+{
+    
+    /**
+     * Konstruktor für Objekte der Klasse View
+     */
+    public View()
+    {
+       
+    }
+
+    
+    public void datenEingeben()
+    {
+        
+    }
+    
+    public void ausgabeTabellarisch()
+    {
+        
+    }
+}
