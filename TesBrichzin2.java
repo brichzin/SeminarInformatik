c9dd059cb5ae156823e5fa34ff4f63b7ce411428
brichzin <peter.brichzin@gymnasium-ottobrunn.de>
2020-04-25 08:52:05 +0200

    Brichzin2 noch lokal enthalten, nochmal hochladen Signed-off-by: brichzin <peter.brichzin@gymnasium-ottobrunn.de>

---
+++ TesBrichzin2.java
@@ -0,0 +1,34 @@
+
+/**
+ * Beschreiben Sie hier die Klasse TesBrichzin2.
+ * 
+ * @author (Manuel Neuer) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class TesBrichzin2
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+    String nachricht;
+    /**
+     * Konstruktor für Objekte der Klasse TesBrichzin2
+     */
+    public TesBrichzin2()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+        nachricht = "Hallo";
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
