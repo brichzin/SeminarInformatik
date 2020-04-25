14a4f3b355deaf24654af7fcb9ba0d6aae75bf20
brichzin <peter.brichzin@gymnasium-ottobrunn.de>
2020-04-25 08:27:53 +0200

    neue Klasse TestBrichzin

---
+++ TestBrichzin.java
@@ -0,0 +1,28 @@
+/**
+ *
+ * Beschreibung
+ *
+ * @version 1.0 vom 25.04.2020
+ * @author 
+ */
+
+public class TestBrichzin {
+  
+  // Anfang Attribute
+  private String nachricht;
+  // Ende Attribute
+  
+  TestBrichzin() 
+  {
+     nachricht= "Hallo";
+  }
+
+  
+  // Anfang Methoden
+  public String getNachricht() {
+    return nachricht;
+  }
+
+  // Ende Methoden
+} // end of TestBrichzin
+
