b7968932a045e301f03d7f2bfa5ca3f720003b87
brichzin <peter.brichzin@gymnasium-ottobrunn.de>
2020-04-26 08:44:13 +0200

    Check Signed-off-by: brichzin <peter.brichzin@gymnasium-ottobrunn.de>

---
+++ TestBrichzin2.java
@@ -0,0 +1,24 @@
+public class TestBrichzin2 
+{
+  
+  // Anfang Attribute
+  private String nachricht;
+  // Ende Attribute
+  
+  TestBrichzin2() 
+  {
+    nachricht= "Hallo!";  
+  }
+
+  
+  // Anfang Methoden
+  public String getNachricht() {
+    return nachricht;
+  }
+  
+  public void setNachricht(String n) {
+    nachricht = n;
+  }
+
+  // Ende Methoden
+} // end of TestBrichzin
