b865cc6333eff6a5c9669b60506b1992ce5122ad
brichzin <peter.brichzin@gymnasium-ottobrunn.de>
2020-04-26 08:35:54 +0200

    Merge Test

---
+++ TestBrichzin.java
@@ -0,0 +1,32 @@
+/**
+ *
+ * Beschreibung
+ *
+ * @version 1.0 vom 25.04.2020
+ * @author 
+ */
+
+public class TestBrichzin {
+  
+  // Anfang Attribute
+  private String nachricht;
+  // Ende Attribute
+  
+  TestBrichzin() 
+  {
+     nachricht= "Hallo!";
+  }
+
+  
+  // Anfang Methoden
+  public String getNachricht() {
+    return nachricht;
+  }
+  
+  public void setNachricht(String n) {
+    nachricht = n;
+  }
+
+  // Ende Methoden
+} // end of TestBrichzin
+
