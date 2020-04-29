3322ac993a9629cf4d40583a50ea37cab4a05f08
Marion Stadtmüller <marion.stadtmueller@gmail.com>
2020-04-29 11:32:41 +0200

    Erste Version MODEL Daten manuell eintragen

--- Model.java
+++ Model.java
@@ -7,20 +7,30 @@
  */
 public class Model
 {
-    
+    int infizierte;
+    int todesfaelle;
+    int tag;
+    int monat;
 
     /**
      * Konstruktor für Objekte der Klasse Model
      */
     public Model()
     {
-       
+       infizierte = 0;
+        todesfaelle = 0;
+        tag = -1;
+        monat = -1;
     }
 
     
-    public void datenManuellEingeben()
+    //Infizierte, Todesfälle, Datum(tag,Monat)
+    public void datenManuellEingeben(int infizierteNeu, int todesfaelleNeu, int tagNeu, int monatNeu)
     {
-        
+        infizierte = infizierteNeu;
+        todesfaelle = todesfaelleNeu;
+        tag = tagNeu;
+        monat = monatNeu;
     }
     
     public void datenAuslesen()
