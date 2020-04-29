4dc8f93dc3cf12f0a65a1d13c44711340ae78f91
Marion Stadtmüller <marion.stadtmueller@gmail.com>
2020-04-29 12:26:01 +0200

    ModelTest erweitert um 5 aktuelle Datensätze

--- ModelTest.java
+++ ModelTest.java
@@ -16,6 +16,13 @@ public class ModelTest
     private Datenpunkt datenpun1;
     private Datenpunkt datenpun2;
     private Model model1;
+    private Datenpunkt datenpun3;
+    private Datenpunkt datenpun4;
+    private Datenpunkt datenpun5;
+
+    
+    
+    
 
     /**
      * Konstruktor fuer die Test-Klasse ModelTest
@@ -35,6 +42,16 @@ public class ModelTest
         datenpun1 = new Datenpunkt(1, 1, 1, 1);
         datenpun2 = new Datenpunkt(2, 2, 2, 1);
         model1 = new Model();
+        datenpun1.datenAktualisieren(2, 0, 28, 01);
+        datenpun2.datenAktualisieren(2, 0, 29, 01);
+        datenpun3 = new Datenpunkt(4, 0, 31, 01);
+        datenpun4 = new Datenpunkt(1, 0, 03, 02);
+        datenpun5 = new Datenpunkt(4, 0, 04, 02);
+        model1.datenpunktEingeben(datenpun1);
+        model1.datenpunktEingeben(datenpun2);
+        model1.datenpunktEingeben(datenpun3);
+        model1.datenpunktEingeben(datenpun4);
+        model1.datenpunktEingeben(datenpun5);
     }
 
     /**
