f34c45de1921bf6cc3e44664f185b6f7093b1e98
Martina Ottinger <martina.ottinger@tum.de>
2020-05-13 14:17:39 +0200

    Zum Testen erstellte Datenpunkt-JSON-Dateien entfernt.

--- JSONCoder.java
+++ JSONCoder.java
@@ -20,9 +20,15 @@ public class JSONCoder
      */
     public JSONCoder()
     {
-        
+
     }
 
+    /**
+     * Methode schreibt den einzelnen übergebenen Datenpunkt in eine  eigene json-Datei. 
+     * Die json-Datei liegt im Ordner Datenpunkte. Der anzugebene Dateiname wird um die Endung json ergänzt. 
+     * @param Dateiname (ohne Endung .json), wie die entstehende json-Datei zu benennen ist.
+     * @param Datenpunkt, der eingelesen werden soll
+     */
     public static void writeDatenpunkt(String filename, Datenpunkt pkt) throws Exception {
         //json-Inhalte zusammenstellen
         JSONObject jo = new JSONObject();
@@ -31,18 +37,24 @@ public class JSONCoder
         jo.put("tag", pkt.tag);
         jo.put("monat", pkt.monat);
         jo.put("jahr", pkt.jahr);
-    
+
         // json-Datei generieren:"JSONExample.json"
         PrintWriter pw = new PrintWriter("Datenpunkte/"+filename+".json"); 
         pw.write(jo.toJSONString()); 
-          
+
         pw.flush(); 
         pw.close(); 
     }
-    
+
+    /**
+     * Methode schreibt alle in einer ArrayList übergebenen Datenpunkte in eine gemeinsame json-Datei. 
+     * Die json-Datei liegt im Ordner Datenpunkte. Der anzugebene Dateiname wird um die Endung json ergänzt. 
+     * @param Dateiname (ohne Endung .json), wie die entstehende json-Datei zu benennen ist.
+     * @param Liste der Datenpunkte, die eingelesen werden sollen
+     */
     public static void write (String filename, ArrayList<Datenpunkt> dp) {
         JSONArray ja = new JSONArray();
-        
+
         for(int i = 0; i < dp.size(); i++) {
             JSONObject jo = new JSONObject();
             jo.put("infizierte", dp.get(i).infizierte);
@@ -52,36 +64,58 @@ public class JSONCoder
             jo.put("jahr", dp.get(i).jahr);
             ja.add(jo);
         }
-        
+
         JSONObject ausgabe = new JSONObject();
         ausgabe.put("datenpunkte", ja);
-        
+
         // json-Datei generieren:"JSONExample.json"
         try{
             PrintWriter pw = new PrintWriter("Datenpunkte/"+filename+".json"); 
             pw.write(ausgabe.toJSONString()); 
-              
+
             pw.flush(); 
             pw.close(); 
         } catch (Exception e) {
             System.out.println (e.toString());
         }
-        
+
     }
-    
-    static Datenpunkt read(String filename) throws Exception  
+
+    /**
+     * Die Methode readDatenpunkt liest den in der json-Datei hinterlegten Datenpunkt ein
+     * und gibt ihn als Objekt der Klasse Datenpunkt zurück. 
+     * Der anzugebene Dateiname wird automatisch um die Endung .json ergänzt und die Datei im Ordner Datenpunkte gesucht.
+     * @param Dateiname (ohne Endung .json), unter dem die json-Datei im Ordner Datenpunkte zu finden ist.
+     * @return Datenpunkt, der eingelesen wurde
+     */
+    static Datenpunkt readDatenpunkt(String filename) throws Exception  
     { 
         // parsing file "JSONExample.json" 
         JSONObject jo = (JSONObject) (new JSONParser().parse(new FileReader("Datenpunkte/"+filename+".json"))); 
-                    
+
         // json auslesen
         int infizierte = (int) (long) jo.get("infizierte"); 
         int todesfaelle = (int) (long) jo.get("todesfaelle");
         int tag = (int) (long) jo.get("tag"); 
         int monat = (int) (long) jo.get("monat"); 
         int jahr = (int) (long) jo.get("jahr"); 
-        
+
         Datenpunkt pkt = new Datenpunkt (infizierte, todesfaelle, tag, monat, jahr); 
         return pkt;
     } 
+
+    
+    /**
+     * Die Methode read liest den in der json-Datei hinterlegte Datenpunkte ein
+     * und gibt sie als ArrayList von Datenpunkten zurück. 
+     * Der anzugebene Dateiname wird automatisch um die Endung .json ergänzt und die Datei im Ordner Datenpunkte gesucht.
+     * @param Dateiname (ohne Endung .json), unter dem die json-Datei im Ordner Datenpunkte zu finden ist.
+     * @return Arraylist der Datenpunkte, die eingelesen wurden
+     */
+    static ArrayList<Datenpunkt> read(String filename) {
+        ArrayList<Datenpunkt> dp = new ArrayList<Datenpunkt>();
+        //to do
+        return dp;
+    }
+    //ToDo 
 }
