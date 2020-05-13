111b6c8e95a3048ec4e6727b2bc02f33c45084d1
Alina Ruggaber <alina.ruggaber@gmail.com>
2020-05-13 11:50:47 +0200

    DBAnbindung implementiert und DB mit SQLite im Ordner

---
+++ DBAnbindung.java
@@ -0,0 +1,46 @@
+import java.sql.*;
+
+public class DBAnbindung
+{
+    Connection c;
+
+    public DBAnbindung()
+    {
+        c = null;
+        init();
+        ausgeben();
+    }
+
+    private void init()  {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:CoronaDaten.db");
+            System.out.println("QSQLite DB connected");
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    private void ausgeben() {
+        try {
+            Statement stat = c.createStatement();
+            ResultSet res = stat.executeQuery("SELECT * FROM CoronaDaten");
+            while (res.next())
+            {
+                System.out.print("Tag: " + res.getInt("Tag") + ", ");
+                System.out.print("Monat: " + res.getInt("Monat") + ", ");
+                System.out.print("Jahr: " + res.getInt("Jahr") + ", ");
+                System.out.print("Infizierte: " + res.getInt("Infizierte") + ", ");
+                System.out.print("Tote: " + res.getInt("Tote"));
+                System.out.println("");
+                
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e);
+
+        }
+
+    }
+}
