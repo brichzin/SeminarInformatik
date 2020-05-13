e3db1793cf0fc55243ea3604c5580255e1fd8f53
Peter <9016605+brichzin@users.noreply.github.com>
2020-05-13 11:43:53 +0200

    Beispielprojekt Json Objekt erzeugen

---
+++ persistenteDatenspeicherung/PersistenteObjekte/Jason/WriteJson.java
@@ -0,0 +1,53 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonWriter;
+
+
+public class WriteJson {
+
+    public static void main(String[] args) {
+        //Json Objekt Erzeuger
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+        // 3 Eintraege
+        builder.add("vorname", "Paul");
+        builder.add("nachname", "Schmitz");
+        builder.add("alter", 14);
+
+        JsonArrayBuilder arrayBuilder1 = Json.createArrayBuilder();
+
+        JsonObjectBuilder job = Json.createObjectBuilder();
+        job.add("stil", "rock");
+        job.add("band", "U2");
+        arrayBuilder1.add(job);
+
+        //bestehender jObjectBuilder wird neu gesetzt
+        job = Json.createObjectBuilder();
+        job.add("stil", "metal");
+        job.add("band", "Black Sabbath");
+        arrayBuilder1.add(job);
+
+        // im Objekt oben wird ein neues Attribut musik ergänzt
+        // dort wird als Attributwert ein Array eingetragen
+        
+        builder.add("musik", arrayBuilder1);
+        builder.add("hungrig", true);
+        
+        // eigentliche Erzeugung des Jason Objekts
+        JsonObject jo = builder.build();
+
+        try {
+            FileWriter fw = new FileWriter("test.txt");
+            JsonWriter jsonWriter = Json.createWriter(fw);
+            jsonWriter.writeObject(jo);
+            jsonWriter.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+} 
