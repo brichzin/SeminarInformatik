8b5c8325958b49c61fc98e9b598810d09d85709a
Alina Ruggaber <alina.ruggaber@gmail.com>
2020-05-27 11:59:11 +0200

    DBAnbindung - Daten aus der View in DB schreiben (ohne Schnittstelle)

--- DBAnbindung.java
+++ DBAnbindung.java
@@ -2,23 +2,36 @@ import java.sql.*;
 
 public class DBAnbindung
 {
-    Connection c;
+    private Connection c;
 
     public DBAnbindung()
     {
-        c = null;
-        init();
-        ausgeben();
-    }
-
-    private void init()  {
         try {
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:CoronaDaten.db");
+            c = DriverManager.getConnection("jdbc:sqlite:" + "./CoronaDaten.db");
             System.out.println("QSQLite DB connected");
         }
         catch (Exception e){
+            e.printStackTrace();
             System.out.println(e);
+        }
+
+        ausgeben();
+    }
+
+    Connection VerbindungGeben ()
+    {
+        return c;
+    }
+
+    void VerbindungBeenden ()
+    {
+        try
+        {
+            c. close ();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
         }
     }
 
@@ -28,14 +41,16 @@ public class DBAnbindung
             ResultSet res = stat.executeQuery("SELECT * FROM CoronaDaten");
             while (res.next())
             {
+                System.out.print("ID: " + res.getInt("ID") + ", ");
                 System.out.print("Tag: " + res.getInt("Tag") + ", ");
                 System.out.print("Monat: " + res.getInt("Monat") + ", ");
                 System.out.print("Jahr: " + res.getInt("Jahr") + ", ");
                 System.out.print("Infizierte: " + res.getInt("Infizierte") + ", ");
                 System.out.print("Tote: " + res.getInt("Tote"));
                 System.out.println("");
-                
+
             }
+            stat.close();
         }
         catch (Exception e) {
             System.out.println(e);
@@ -43,17 +58,18 @@ public class DBAnbindung
         }
 
     }
-    /*private void inDatenbankEingeben(int TagNeu, int MonatNeu, int JahrNeu, int InfizierteNeu, int ToteNeu) {
+
+    private void inDatenbankEingeben(int TagNeu, int MonatNeu, int JahrNeu, int InfizierteNeu, int ToteNeu) {
         try {
             Statement stat = c.createStatement();
             int Anzahl=stat.executeUpdate("INSERT INTO CoronaDaten"+
-            "VALUES ( TagNeu, MonatNeu, JahrNeu, InfizierteNeu, ToteNeu)");
-            
+                    "VALUES ( TagNeu, MonatNeu, JahrNeu, InfizierteNeu, ToteNeu)");
+
         }
         catch (Exception e) {
             System.out.println(e);
 
         }
 
-    }*/
+    }
 }
